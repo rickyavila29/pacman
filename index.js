c4b94ac9deee724c2e41f4cda73030612748313b
Ace <106693166+rickyavila29@users.noreply.github.com>
2023-06-29 12:25:00 -0600

    Added boundary! Realized I defined width twice instead of height and was using this.position.width instead of this.width... blue boundary is now inbound :)

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
 const canvas = document.querySelector('canvas')
 
-const context = canvas.getContext("2d")
+const ctx = canvas.getContext("2d");
 
 canvas.width = innerWidth
-canvas.width = innerHeight
+canvas.height = innerHeight
+
+class Boundary {
+constructor ({position}){
+    this.position = position
+    this.width = 40
+    this.height = 40
+};
+draw() {
+    ctx.fillStyle = "blue" //what is the main difference between ' and "?
+    ctx.fillRect(this.position.x,this.position.y,// a.b.c why do these statements make sense logically?
+        this.width, this.height
+    )
+    };
+};
+
+const boundary = new Boundary ({
+position: {
+    x: 0,
+    y: 0
+}
+});
+
+boundary.draw()
