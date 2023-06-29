3616260b743cd8f02720a9ad205f7f81ac227835
Ace <106693166+rickyavila29@users.noreply.github.com>
2023-06-29 12:42:24 -0600

    Added multiple boundaries

--- index.js
+++ index.js
@@ -1,29 +1,42 @@
-const canvas = document.querySelector('canvas')
-
+const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext("2d");
 
-canvas.width = innerWidth
-canvas.height = innerHeight
+canvas.width = innerWidth;
+canvas.height = innerHeight;
 
 class Boundary {
-constructor ({position}){
-    this.position = position
-    this.width = 40
-    this.height = 40
-};
-draw() {
-    ctx.fillStyle = "blue" //what is the main difference between ' and "?
-    ctx.fillRect(this.position.x,this.position.y,// a.b.c why do these statements make sense logically?
-        this.width, this.height
-    )
-    };
-};
+  constructor({ position }) {
+    this.position = position;
+    this.width = 40;
+    this.height = 40;
+  }
 
-const boundary = new Boundary ({
-position: {
-    x: 0,
-    y: 0
+  draw() {
+    ctx.fillStyle = "blue";
+    ctx.fillRect(
+      this.position.x,
+      this.position.y,
+      this.width,
+      this.height
+    );
+  }
 }
-});
 
-boundary.draw()
+const boundaries = [
+    new Boundary({
+        position: {
+          x: 0,
+          y: 0
+        }
+      }),
+      new Boundary({
+        position: {
+          x: 41,
+          y: 0
+        }
+      })
+      
+]
+boundaries.forEach((boundary) => {
+    boundary.draw()
+})
